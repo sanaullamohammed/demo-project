de8ed709d8ba04ad596aa8be91283bab24aff435
sanaullamohammed <sanaullamohammed177@gmail.com>
2024-09-22 14:27:11 +0530

    demo-project

---
+++ src/main/java/com/neoskills/demo_project/App.java
@@ -0,0 +1,10 @@
+package com.neoskills.demo_project;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
